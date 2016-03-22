254f5c23896fccfa30e93cf6c8fd3dddbb757f7e
Grzegorz Grasza <grzegorz.grasza@intel.com>
2016-03-22 20:16:42 +0100

    Working Tsung test

--- Makefile
+++ Makefile
@@ -1,7 +1,11 @@
 SOURCES=templates/tsung.xml
 CONFIGS=pgsql mysql
+TESTS=tsung-mysql tsung-pgsql
 
 all: $(CONFIGS)
 
 $(CONFIGS): $(SOURCES)
 	python generate.py $@
+
+$(TESTS):
+	tsung -f $@.xml -m - start
--- generate.py
+++ generate.py
@@ -14,8 +14,10 @@ if __name__ == '__main__':
     sql = argv[1]
     assert sql in ('pgsql', 'mysql')
     if sql == 'mysql':
-        generate(sql, port=3306)
+        generate(sql, port=3306, database='mysql',
+                 user='root', password='')
     elif sql == 'pgsql':
-        generate(sql, port=5432)
+        generate(sql, pg=True, port=5432, database='template1',
+                 user='postgres', password='empty')
     else:
         exit(1)
